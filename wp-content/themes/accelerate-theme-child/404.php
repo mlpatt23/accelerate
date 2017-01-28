289987401f6bd9b28ef8486ef15c6fb400887d5f
Melanie <mlpatt23@gmail.com>
2017-01-27 19:09:23 -0700

    created and styled 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the 404 page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<section class="not-found-page">
+		<div class="site-content">
+			<img src="http://localhost:8888/accelerate/wp-content/uploads/2017/01/goat.jpg" alt="goat image">
+			
+			<div class="not-found-description">
+				<h1>AHHHHHHH!</h1>
+			
+				<p>This page no longer exists, never existed, or has been moved.</p>
+			
+				<p>Feel free to take a look around our <a href="" alt="blog" class="in-text-link">blog</a> or some of our featured <a href="" alt="work" class="in-text-link">work</a>.
+					
+				<p>Or you know, just let it out.</p>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer(); ?>
